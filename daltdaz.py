1484281648a0b8f29a3e5f2a6824386f217cf592
gastronomyK <kdleschinski@gmail.com>
2015-11-05 07:48:55 +0100

    Putting the functions in Poynter_module, altaz and daltdaz are interface files

--- daltdaz.py
+++ daltdaz.py
@@ -8,14 +8,19 @@ if len(argv) == 2:
 	lat = -31.27703889
 	print("Assuming that we're in Sydney", lon, lat)
 	
-	a = finger.get_altaz(obj, lon, lat)
+	a = finger.get_daltdaz(obj, lon, lat)
 	print(a[0], a[1])
 	
 elif len(argv) == 4:
 	obj, lon, lat, dt = argv[1], argv[2], argv[3]
 	
-	a = finger.get_altaz(obj, lon, lat)
+	a = finger.get_daltdaz(obj, lon, lat)
 	print(a[0], a[1])
 	
+elif len(argv) == 5:
+	obj, lon, lat, dt = argv[1], argv[2], argv[3], argv[4]
+	
+	a = finger.get_daltdaz(obj, lon, lat, dt)
+	print(a[0], a[1])
 	
 else: print("The proper syntax is \n >> python altaz.py ObjectName Longitude Latitude")
