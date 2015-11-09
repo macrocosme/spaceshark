72dea0e2810278d9abfe5e1682fa1582f200b985
macrocosme <danyvohl@hotmail.com>
2015-11-10 08:55:59 +1100

    AJAX requests, Merged templates and Tracking function modified
    Changed the behaviour so that we don’t reload the page when we click
    « point! » by using AJAX.
    
    Also, I merged index.html and layout.html and got rid of index.html.
    
    Finally, I modified the way I call the tracking function. It now calls
    the astropointer.py with extra parameters in a different forked
    process. This works once, but won’t work when we change object. THIS
    NEEDS TO BE MODIFIED.

--- astropointer.py
+++ astropointer.py
@@ -1,14 +1,19 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from poynter_module import get_altaz, get_daltdaz
 import requests
 from os import system, fork
 import sched, time
+from subprocess import Popen
+import random
+import sys
 
 s = sched.scheduler(time.time, time.sleep)
 API_KEY = 'AIzaSyCY8AUysBMDz0d20GuIUaMbyJrr6pL-RYQ'
 global PROCESSES
 PROCESSES = []
-DELAY = 60
+DELAY = 60.
+
+rand = random.random()
 
 app = Flask(__name__)
 
@@ -18,52 +23,72 @@ class Objects:
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
-    if request.method == 'POST':
-        try:
-            if request.form['temp'] == 'List':
-                object = request.form['object_list']
-            else:
-                object = request.form['object_textbox']
-
-            device_id = request.form['device_id']
-            access_token = request.form['access_token']
-            address = request.form['address']
-
-
-            # Retrieve address from google map
-            response_get = requests.get('https://maps.googleapis.com/maps/api/geocode/json',
-                            params={'address': address, 'key': API_KEY})
-            location = response_get.json()['results'][0]['geometry']['location']
-
-            # Convert coordinates (lng, lat) --> (alt, az)
-            alt, az = get_altaz(object, location['lng'], location['lat'])
-            # Send command to clouddy hardware for alt
-            system('curl https://api.particle.io/v1/devices/'+str(device_id)+'/point_alt ' \
-                   '-d access_token='+str(access_token)+' ' \
-                   '-d "args='+str(alt)+'"')
-
-            system('curl https://api.particle.io/v1/devices/'+str(device_id)+'/point_az ' \
-                   '-d access_token='+str(access_token)+' ' \
-                   '-d "args='+str(az)+'"')
-
-            # Convert coordinates (lng, lat) --> (alt, az)
-            d_alt, d_az = get_daltdaz(object, location['lng'], location['lat'])
-            #  d_alt, d_az = d_alt*1000, d_az*1000
-            #  print(d_alt, d_az)
-            if len(PROCESSES) > 0:
-                os.kill(PROCESSES[0], signal.SIGKILL)
-            pid = fork()
-            PROCESSES.append(pid)
-            s.enter(DELAY, 1, delta_update, (s, device_id, access_token, d_alt, d_az,))
-            s.run()
-            sys.exit()
-
-        except:
-            print("oops.")
     return render_template('index.html', objects=Objects.objects)
 
-def delta_update(sc, device_id, access_token, d_alt, d_az):
+@app.route("/_point")
+def point():
+    try:
+        print "allo."
+        print request.args.get('temp')
+
+        if request.args.get('temp') == 'List':
+            object = request.args.get('object_list')
+        else:
+            object = request.args.get('object_textbox')
+
+        device_id = request.args.get('device_id')
+        access_token = request.args.get('access_token')
+        address = request.args.get('address')
+
+        """
+        # Retrieve address from google map
+        response_get = requests.get('https://maps.googleapis.com/maps/api/geocode/json',
+                        params={'address': address, 'key': API_KEY})
+        location = response_get.json()['results'][0]['geometry']['location']
+
+        # Convert coordinates (lng, lat) --> (alt, az)
+        alt, az = get_altaz(object, location['lng'], location['lat'])
+        # Send command to clouddy hardware for alt
+        system('curl https://api.particle.io/v1/devices/'+str(device_id)+'/point_alt ' \
+               '-d access_token='+str(access_token)+' ' \
+               '-d "args='+str(alt)+'"')
+
+        system('curl https://api.particle.io/v1/devices/'+str(device_id)+'/point_az ' \
+               '-d access_token='+str(access_token)+' ' \
+               '-d "args='+str(az)+'"')
+
+        # Convert coordinates (lng, lat) --> (alt, az)
+        d_alt, d_az = get_daltdaz(object, location['lng'], location['lat'])
+        #  d_alt, d_az = d_alt*1000, d_az*1000
+        #  print(d_alt, d_az)
+        """
+
+        if len(PROCESSES) > 0:
+            for process in PROCESSES:
+                print("process:", process)
+                try:
+                    os.kill(process, signal.SIGKILL)
+                    print "after"
+                except OSError, e:
+                    print "error: ", e.errno
+
+        #p = Popen(['python', 'astropointer.py', str(device_id), str(access_token), str(d_alt), str(d_az), str(DELAY)])
+        p = Popen(['python', 'astropointer.py', str(device_id), str(access_token), str(1), str(1), str(DELAY)])
+
+        PROCESSES.append(p.pid)
+    except:
+        print("oops.")
+
+    return jsonify(response="ok")
+
+def delta_update(device_id, access_token, d_alt, d_az, delay):
     print('delta_update...')
+    print(rand)
+
+    #time.sleep(delay)
+    time.sleep(1.)
+    delta_update(device_id, access_token, d_alt, d_az, delay)
+    """
     # Send command to clouddy hardware for alt
     system('curl https://api.particle.io/v1/devices/'+str(device_id)+'/track_alt ' \
                                                                     '-d access_token='+str(access_token)+' ' \
@@ -72,7 +97,12 @@ def delta_update(sc, device_id, access_token, d_alt, d_az):
     system('curl https://api.particle.io/v1/devices/'+str(device_id)+'/track_az ' \
                                                                     '-d access_token='+str(access_token)+' ' \
                                                                     '-d "args='+str(d_az)+'"')
-    sc.enter(DELAY, 1, delta_update, (sc, device_id, access_token, d_alt, d_az,))
+    """
+    #sc.enter(delay, 1, delta_update, (sc, device_id, access_token, d_alt, d_az,))
 
 if __name__ == "__main__":
-    app.run()
+    if len(sys.argv) == 1:
+        app.run()
+    else:
+        print len(sys.argv)
+        delta_update(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
